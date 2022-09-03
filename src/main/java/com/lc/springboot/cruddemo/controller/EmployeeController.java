678d55297bb54496e1464dbd90a190796831e3dd
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-03 19:04:26 +0300

    progress update

--- src/main/java/com/lc/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/lc/springboot/cruddemo/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lc.springboot.cruddemo.entity.Employee;
 import com.lc.springboot.cruddemo.service.EmployeeService;
@@ -31,6 +32,27 @@ public class EmployeeController {
 		theModel.addAttribute("employees", theEmployees);
 		
 		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, 
+			Model theModel){
+		
+		Employee theEmployee = new Employee();
+
+		theEmployee = employeeService.findById(theId);
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId){
+
+		employeeService.deleteById(theId);
+			
+		return "redirect:/employees/list";
 	}
 	
 	@GetMapping("/showFormForAdd")
