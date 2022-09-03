55e219b95e95d899d5077bb018300a3da2073ae2
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-03 18:34:04 +0300

    progress update

--- src/main/java/com/lc/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/lc/springboot/cruddemo/controller/EmployeeController.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lc.springboot.cruddemo.entity.Employee;
 import com.lc.springboot.cruddemo.service.EmployeeService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/employees")
 public class EmployeeController {
 
 	private EmployeeService employeeService;
@@ -28,7 +30,24 @@ public class EmployeeController {
 		List<Employee> theEmployees = employeeService.findAll();
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel){
+		
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+		
+		employeeService.save(theEmployee);
+		
+		return "redirect:/employees/list";
 	}
 /*
 	@GetMapping("/employees/{employeeId}")
--- src/main/java/com/lc/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/lc/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.lc.springboot.cruddemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -7,5 +9,7 @@ import com.lc.springboot.cruddemo.entity.Employee;
 
 @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	public List<Employee> findAllByOrderByLastNameAsc();
 
 }
