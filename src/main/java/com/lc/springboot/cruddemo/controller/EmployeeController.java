8f76f168acc7886f087c172553bd09d66da9bbcb
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-03 19:05:04 +0300

    progress update

--- src/main/java/com/lc/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/lc/springboot/cruddemo/controller/EmployeeController.java
@@ -71,53 +71,5 @@ public class EmployeeController {
 		
 		return "redirect:/employees/list";
 	}
-/*
-	@GetMapping("/employees/{employeeId}")
-	public Employee findEmployee(@PathVariable int employeeId){
-
-		Employee theEmployee = employeeService.findById(employeeId);
-
-		if (theEmployee == null) {
-			throw new RuntimeException("Employee id not found - " + employeeId);
-		}
-
-		return theEmployee;
-	}
-
-	@PostMapping("/employees")
-	public Employee addEmployee(@RequestBody Employee theEmployee){
-
-		theEmployee.setId(0);
-
-		employeeService.save(theEmployee);
-
-		return theEmployee;
-
-	}
-
-	@PutMapping("/employees")
-	public Employee updateEmployee(@RequestBody Employee theEmployee){
-
-		employeeService.save(theEmployee);
-
-		return theEmployee;
-
-	}
-
-	@DeleteMapping("/employees/{employeeId}")
-	public String deleteEmployee(@PathVariable int employeeId){
-
-		Employee theEmployee = employeeService.findById(employeeId);
-
-		if (theEmployee == null) {
-			throw new RuntimeException("Employee id: " + employeeId + " not found");
-		}
-
-		employeeService.deleteById(employeeId);
-
-		return "Deleted employee id " + employeeId;
-
-	}
-*/
 
 }
