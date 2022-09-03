16bb2de1ae8a72fd4909ebda356548340b04e0b0
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-03 12:02:18 +0300

    progress update

--- src/main/java/com/lc/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/lc/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.lc.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.lc.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
